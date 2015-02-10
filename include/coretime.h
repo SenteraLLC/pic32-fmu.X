b925eff5c7d5933b59fc032be235319d1c341cc4
Brian Eickhoff <BrianEickhoff@users.noreply.github.com>
2015-02-09 23:28:19 -0600

    Initial core timer module.

---
+++ include/coretime.h
@@ -0,0 +1,33 @@
+/*******************************************************************************
+/
+/   Filename:   coretime.h
+/
+*******************************************************************************/
+
+#ifndef CORETIME_H
+#define	CORETIME_H
+
+#include <stdint.h>
+
+
+// Typedefs ====================================================================
+
+#define CORETIME_TICKS_PER_SECOND       40000000UL
+
+
+
+// Function Prototypes =========================================================
+
+uint64_t CoreTimeTicksGet();    // Get unitless 64-bit core timer tick count.
+
+uint64_t CoreTime64sGet();      // Get 64-bit core time in seconds.
+uint64_t CoreTime64msGet();     // Get 64-bit core time in milliseconds.
+uint64_t CoreTime64usGet();     // Get 64-bit core time in microseconds.
+
+uint32_t CoreTime32sGet();      // Get 32-bit core time in seconds.
+uint32_t CoreTime32msGet();     // Get 32-bit core time in milliseconds.
+uint32_t CoreTime32usGet();     // Get 32-bit core time in microseconds.
+
+
+#endif	/* CORETIME_H */
+
---
+++ source/coretime.c
@@ -0,0 +1,89 @@
+/*******************************************************************************
+/
+/   Filename:   coretime.c
+/
+*******************************************************************************/
+
+#include <xc.h>
+#include <sys/attribs.h>
+#include "coretime.h"
+#include "stdtypes.h"
+
+
+static uint32_t coreTimeOverflow;   // Core timer overflow counter.
+                                    // Used to provide 64-bit system time.
+
+
+//==============================================================================
+
+uint64_t CoreTimeTicksGet()
+{
+    UINT_64 ticks;
+
+    do {
+        IEC0SET = _IEC0_CTIE_MASK;              // Enable CT interrupts.
+        Nop();
+        IEC0CLR = _IEC0_CTIE_MASK;              // Disable CT interrupts.
+        ticks.dwords.dword0 = _mfc0(_CP0_COUNT,
+                _CP0_COUNT_SELECT);             // Fetch 32-bit CT register.
+        ticks.dwords.dword1 =
+                coreTimeOverflow;               // Fetch 32-bit CT overflow.
+    } while ((IFS0 & _IFS0_CTIF_MASK) != 0);
+
+    IEC0SET = _IEC0_CTIE_MASK;                  // Enable CT interrupts.
+
+    return ticks.val;
+}
+
+//==============================================================================
+
+uint64_t CoreTime64sGet()
+{
+    return CoreTimeTicksGet() / CORETIME_TICKS_PER_SECOND;
+}
+
+//==============================================================================
+
+uint64_t CoreTime64msGet()
+{
+    return CoreTimeTicksGet() / (CORETIME_TICKS_PER_SECOND / 1000);
+}
+
+//==============================================================================
+
+uint64_t CoreTime64usGet()
+{
+    return CoreTimeTicksGet() / (CORETIME_TICKS_PER_SECOND / 1000000);
+}
+
+//==============================================================================
+
+uint32_t CoreTime32sGet()
+{
+    return CoreTimeTicksGet() / CORETIME_TICKS_PER_SECOND;
+}
+
+//==============================================================================
+
+uint32_t CoreTime32msGet()
+{
+    return CoreTimeTicksGet() / (CORETIME_TICKS_PER_SECOND / 1000);
+}
+
+//==============================================================================
+
+uint32_t CoreTime32usGet()
+{
+    return CoreTimeTicksGet() / (CORETIME_TICKS_PER_SECOND / 1000000);
+}
+
+//==============================================================================
+
+// Core Timer ISR
+
+void __ISR (_CORE_TIMER_VECTOR, IPL7SRS) CoreTimerISR(void)
+{
+    coreTimeOverflow++;             // Increment core timer overflow count.
+    IFS0CLR = _IFS0_CTIF_MASK;      // Clear core timer interrupt flag.
+}
+

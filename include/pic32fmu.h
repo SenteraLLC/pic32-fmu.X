5fe423c07653ffcec15e2e488c91f4d93d479f3d
Brian Eickhoff <BrianEickhoff@users.noreply.github.com>
2015-02-16 16:04:58 -0600

    Initial.

---
+++ include/pic32fmu.h
@@ -0,0 +1,22 @@
+/*******************************************************************************
+/
+/   Filename:   pic32fmu.h
+/
+*******************************************************************************/
+
+
+#ifndef PIC32FMU_H
+#define	PIC32FMU_H
+
+#include "cfgbits.h"        // Device configuration bits.
+
+#include "spi.h"
+#include "stdtypes.h"
+#include "coretime.h"
+#include "init.h"
+#include "vn100.h"
+#include "oemstar.h"
+
+
+#endif	/* PIC32FMU_H */
+

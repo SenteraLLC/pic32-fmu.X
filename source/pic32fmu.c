6c374118aad417fe1a5d3c7489ce483e171de80e
Brian Eickhoff <BrianEickhoff@users.noreply.github.com>
2015-02-16 16:04:25 -0600

    Added tasks to the main program loop.

--- source/pic32fmu.c
+++ source/pic32fmu.c
@@ -4,20 +4,29 @@
 /
 *******************************************************************************/
 
-#include "cfgbits.h"        // Device configuration bits.
-
-#include <p32xxxx.h>
+#include <xc.h>
+#include "pic32fmu.h"
 
 
 //==============================================================================
 
 int main()
 {
-    for (;;)
-    {
-        WDTCONSET = _WDTCON_WDTCLR_MASK;    // Clear watchdog timer.
-    }
+    InitBoard();    // Initialize FMU processor and peripherals.
     
+    for (;;)        // Main program loop.
+    {
+        WDTCONSET = _WDTCON_WDTCLR_MASK;        // Clear watchdog timer.
+        LATFbits.LATF3 = CoreTime64sGet() % 2;  // Blink LED at 0.5Hz
+
+        // Low-level communication tasks. -----------------
+        SPITask();
+
+        // Acquire sensor data. ---------------------------
+        VN100Task();
+        OEMStarTask();
+    }
+
     return 0;
 }
 

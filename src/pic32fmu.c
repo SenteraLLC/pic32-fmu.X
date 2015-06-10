17b3819852f251e3055132ece74a494c3def67fa
BrianEickhoff <brian.eickhoff@sentera.com>
2015-06-10 14:42:50 -0500

    Updated TCPIP stack supporting functions to the main program loop.

--- src/pic32fmu.c
+++ src/pic32fmu.c
@@ -5,7 +5,18 @@
 *******************************************************************************/
 
 #include <xc.h>
-#include "pic32fmu.h"
+#include "spi.h"
+#include "stdtypes.h"
+#include "coretime.h"
+#include "init.h"
+#include "vn100.h"
+#include "oemstar.h"
+
+// Include all headers for any enabled TCPIP Stack functions
+#include "tcpip/tcpip.h"
+
+// Declare AppConfig structure and some other supporting stack variables
+APP_CONFIG AppConfig;
 
 
 //==============================================================================
@@ -19,12 +30,20 @@ int main()
         WDTCONSET = _WDTCON_WDTCLR_MASK;        // Clear watchdog timer.
         LATFbits.LATF3 = CoreTime64sGet() % 2;  // Blink LED at 0.5Hz
 
-        // Low-level communication tasks. -----------------
+        // Low-level communication tasks. ---------------------------
         SPITask();
 
-        // Acquire sensor data. ---------------------------
+        // Acquire sensor data. -------------------------------------
         VN100Task();
         OEMStarTask();
+        
+        // This task performs normal stack task including checking
+        // for incoming packet, type of packet and calling
+        // appropriate stack entity to process it.
+        StackTask();
+
+        // This tasks invokes each of the core stack application tasks.
+        StackApplications();
     }
 
     return 0;
